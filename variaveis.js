7c8c67bbdafd301b18e4f2b8ccaa10dcd36b5464
Maurício Lourenço de Aguiar - 82212145 <82212145@ulife.com.br>
2025-03-11 10:12:32 -0300

    Feat: Trabalhando com variáveis

---
+++ variaveis.js
@@ -0,0 +1,31 @@
+var idade = 39;
+let nome = "Maumau";
+const PI = 3.14;
+let ativo = true;
+
+//Conversão de String para Número, lembre o ";" Não é obrigatório ao final de cada linha
+let num = Number("42")
+let float = parseFloat("5.7")
+let inteiro = parseInt("10.5")
+
+//Conversão de Número para String
+
+let str = String(100)
+let outraStr = (42).toString()
+
+console.log(idade)
+console.log(nome)
+console.log(PI)
+console.log(ativo)
+console.log(inteiro)
+console.log("String: " +str)
+console.log("String: " + outraStr)
+
+if (true) {
+    console.log(idade)
+    console.log(nome)
+    console.log(PI)
+    console.log(ativo)
+    
+}
+
