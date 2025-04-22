0a3cc11b960ceb5d819e9951aeac3ed3b81a875c
Maurício Lourenço de Aguiar - 82212145 <82212145@ulife.com.br>
2025-04-22 10:14:07 -0300

    Adição do put e delete

--- microsservices-rest/order-service/orderServices.js
+++ microsservices-rest/order-service/orderServices.js
@@ -16,6 +16,35 @@ app.post('/pedidos', (req,res) =>{ // o  post vai adicionar, ele vai mandar.
     orders.push(newOrder); //add the new order to the array
     res.status(201).send({ message: 'Pedido cadastrado com sucesso!', order: newOrder});
 
+});
+
+app.get('/pedidos', (req,res) =>{
+    res.status(200).json(orders) //return the list of orders
+});
+
+app.get('/pedidos/:id', (req,res) =>{
+    const orderId = req.params.id; //get the order id from the request params
+    const order = orders.find(o => o.id === orderId); //find the order in the array by id
+    // o find vai retornar o primeiro elemento que satisfaz a condição, se não encontrar retorna undefined
+    if(!order){
+        return res.status(404).send({ message: 'Pedido não encontrado!'});
+    }
+    res.status(200).json(order); //return the order
+})
+
+app.put('/pedidos/:id', (req,res) =>{
+    const orderId = req.params.id; //get the order id from the request params
+    const { product, quantity } = req.body; // destructure the order object
+    const orderIndex = orders.findIndex(o => o.id === orderId); //find the order in the array by id
+    if(orderIndex === -1){
+        return res.status(404).send({ message: 'Pedido não encontrado!'});
+    }
+    if(!product || !quantity){
+        return res.status(400).send({ message: "Dados do pedido inválidos!"});
+    
+    }
+    orders[orderIndex] = { id: orderId, product, quantity }; //update the order in the array
+    res.status(200).send({ message: 'Pedido atualizado com sucesso!', order: orders[orderIndex]});
 })
 
 app.listen(4000, () => console.log ('Order Service running on port 4000'))
