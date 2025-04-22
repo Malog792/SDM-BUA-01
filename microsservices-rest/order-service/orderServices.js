c860fa82c2875f87657eb9d6547f6ef5fb6773c0
Maurício Lourenço de Aguiar - 82212145 <82212145@ulife.com.br>
2025-04-22 09:48:25 -0300

    Adição de status do pedido

--- microsservices-rest/order-service/orderServices.js
+++ microsservices-rest/order-service/orderServices.js
@@ -1,14 +1,21 @@
 const express = require('express')
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json()) // o metodo use chama outro medoto que é o express.json, seria o metodo que diz o tipo de dados será utilizado, como o json
 
-app.post('/pedidos', (req,res) =>{
-    const pedido = req.body
+let orders = []; // array to store orders
+
+app.post('/pedidos', (req,res) =>{ // o  post vai adicionar, ele vai mandar.
+
+    const {id, product, quantity} = req.body; // destructure the order object
+    if(!id || !product || !quantity){
+        return res.status(400).send({ message: "Dados do pedido inválidos!"});
     
-    console.log("Pedido solicitado", pedido)
-    res.send({message: 'Pedido solicitado com sucesso', pedido})
+    }
+    const newOrder = (id, product, quantity); // Create a new Order object
+    orders.push(newOrder); //add the new order to the array
+    res.status(201).send({ message: 'Pedido cadastrado com sucesso!', order: newOrder});
 
 })
 
-app.listen(4000, () => console.log ('User Service running on port 4000'))
+app.listen(4000, () => console.log ('Order Service running on port 4000'))
